6291ba759b65cf6d728a695ce6211395ca4c2327
Wizard Bot 86 <harshit.whitehat@gmail.com>
2021-12-18 18:01:21 +0530

    Add files via upload

---
+++ CW/Virtual-Tour/components/tour.js
@@ -0,0 +1,89 @@
+AFRAME.registerComponent("tour", {
+  schema: {},
+  init: function () {
+    this.placesContainer = this.el;
+    this.createCards();
+  },
+  createCards: function () {
+    const thumbnailsRef = [
+      {
+        id: "taj-mahal",
+        url: "./thumbnails/taj_mahal.png",
+        title: "Taj Mahal",
+      },
+      {
+        id: "budapest",
+        url: "./thumbnails/budapest.jpg",
+        title: "Budapest",
+      },
+      {
+        id: "eiffel-tower",
+        url: "./thumbnails/eiffel_tower.jpg",
+        title: "Eiffel Tower",
+      },
+      {
+        id: "new-york",
+        url: "./thumbnails/new_york_city.png",
+        title: "New York",
+      },
+    ];
+    var previousXPosition = -63;
+    for (var item of thumbnailsRef) {
+      const posX = previousXPosition + 25;
+      const posY = -5;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      previousXPosition = posX;
+
+      const borderE1 = this.createBorder(position, item.id);
+      const thumbnailE1 = this.createThumbnail(item);
+      borderE1.appendChild(thumbnailE1);
+      const titleE1 = this.createTitle(item, position);
+      borderE1.appendChild(titleE1);
+
+      this.placesContainer.appendChild(borderE1);
+    }
+  },
+  createBorder: function (pos, id) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "ring",
+      radiusInner: 9,
+      radiusOuter: 10,
+    });
+    entityEl.setAttribute("position", pos);
+    entityEl.setAttribute("material", {
+      color: "#00bcd3",
+    });
+    return entityEl;
+  },
+  createThumbnail: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "circle",
+      radius: 9,
+    });
+    entityEl.setAttribute("material", {
+      src: item.url,
+    });
+    return entityEl;
+  },
+  createTitle: function (item, position) {
+    const titleE1 = document.createElement("a-entity");
+    titleE1.setAttribute("visible", true);
+    titleE1.setAttribute("text", {
+      font: "exo2bold",
+      align: "center",
+      width: 100,
+      color: "#000",
+      value: item.title,
+    });
+    const elPosition = position;
+    elPosition.y -= 30;
+    titleE1.setAttribute("position", elPosition);
+    return titleE1;
+  },
+});
