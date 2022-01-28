36f4fa2b98e03b176f4f9867f50acbb5ca7c8cb0
Joe Metrically Ok <harshit.whitehat@gmail.com>
2022-01-28 08:17:54 +0530

    Add files via upload

---
+++ CW/Shooting-Game/shoot.js
@@ -0,0 +1,31 @@
+AFRAME.registerComponent("bullets", {
+  init: function () {
+    this.shootBullet();
+  },
+  shootBullet: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.code == "Space") {
+        this.createBullet();
+      }
+    });
+    window.addEventListener("mousedown", () => {
+      this.createBullet();
+    });
+  },
+  createBullet: function () {
+    let bulletEl = document.createElement("a-sphere");
+    bulletEl.setAttribute("radius", "0.1");
+    bulletEl.setAttribute("color", "black");
+    bulletEl.setAttribute("shader", "flat");
+
+    let camPos = document.querySelector("#camera").getAttribute("position");
+    bulletEl.setAttribute("position", camPos);
+
+    let direction = new THREE.Vector3();
+    document.querySelector("#camera").object3D.getWorldDirection(direction);
+    bulletEl.setAttribute("velocity", direction.multiplyScalar(-15));
+
+    let sceneEl = document.querySelector("#scene");
+    sceneEl.appendChild(bulletEl);
+  },
+});
