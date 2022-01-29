35446deb7271cf47d7afd7131aa13c00f9f1f6be
Joe Metrically Ok <harshit.whitehat@gmail.com>
2022-01-29 17:47:50 +0530

    Add files via upload

--- CW/Shooting-Game/shoot.js
+++ CW/Shooting-Game/shoot.js
@@ -24,12 +24,38 @@ AFRAME.registerComponent("bullets", {
     let direction = new THREE.Vector3();
     document.querySelector("#camera").object3D.getWorldDirection(direction);
     bulletEl.setAttribute("velocity", direction.multiplyScalar(-15));
-
+    bulletEl.setAttribute("dynamic-body", {
+      shape: "sphere",
+      mass: "0",
+    });
+    bulletEl.addEventListener("collide", this.removeBullet);
+    
     let sceneEl = document.querySelector("#scene");
     sceneEl.appendChild(bulletEl);
+  },
+  removeBullet: function (e) {
+    let el = e.detail.target.el;
+    let elHit = e.detail.body.el;
 
-    setTimeout(()=>{
-      bulletEl.remove();
-    }, 5000)
+    console.log("The target is:", el);
+    console.log("The bullet hit:", elHit);
+    if (elHit.id.includes("box")) {
+      elHit.setAttribute("material", {
+        opacity: "0.5",
+        transparent: "true",
+      })
+    }
+    // Cannon.js is the physics engine over which the AFRAME 3D physics component is based
+    // applyImpulse(impulse, Worldpoint)
+    let impulse = new CANNON.Vec3(-2, 2, 1);
+    let worldpoint = new CANNON.Vec3().copy(elHit.getAttribute("position"))
+    applyImpulse(impulse, worldpoint);
+
+    el.removeEventListener("collide", this.shoot);
+
+    let sceneEl = document.querySelector("#scene");
+    if(el.parent == sceneEl) {
+      sceneEl.removeChild(el);
+    }
   },
 });
