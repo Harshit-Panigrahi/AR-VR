1032ad1fc76b4c7265ed6071241856fec40b64d6
Joe Metrically Ok <harshit.whitehat@gmail.com>
2022-06-01 17:59:10 +0530

    Add files via upload

--- Self-Designed-Project/component.js
+++ Self-Designed-Project/component.js
@@ -1,12 +1,20 @@
 let menuVis = false;
+let shape = "cube";
 let canvasEl;
+let color;
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
 AFRAME.registerComponent("controls", {
   init: function () {
     this.toggleMenu();
+    this.placeShape();
   },
   tick: function () {
     this.toggleflight();
+    this.getColor();
+    this.getShape();
   },
   toggleflight: () => {
     if ($("#flight-switch").prop("checked")) {
@@ -16,6 +24,17 @@ AFRAME.registerComponent("controls", {
       $("a-camera").attr("wasd-controls", "fly: false");
       $("#flight-mode").html("Flight mode: Disabled");
     }
+  },
+  getColor: () => {
+    let colorPicker = document.getElementById("color-picker");
+    color = $(colorPicker).val();
+  },
+  getShape: () => {
+    $(".shape").click(function (e) {
+      $(".shape").attr("selected", false);
+      $(this).attr("selected", true);
+      shape = $(this).attr("alt");
+    });
   },
   toggleMenu: () => {
     canvasEl = document.querySelector("canvas");
@@ -33,5 +52,13 @@ AFRAME.registerComponent("controls", {
       }
     });
   },
-  
+  placeShape: () => {
+    const scene = document.querySelector("a-scene");
+    const camera = document.querySelector("a-camera");
+    window.addEventListener("click", (e) => {
+      raycaster.setFromCamera(pointer, camera);
+      const intersects = raycaster.intersectObjects(scene.children);
+      console.log(intersects);
+    });
+  },
 });
